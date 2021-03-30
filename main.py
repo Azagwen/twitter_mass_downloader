d338309d7a693078a8f80c607ecd0e4740b5aa5f
Azagwen <30528331+Azagwen@users.noreply.github.com>
2021-03-30 15:23:50 +0200

    Modified input to allow sub-folder specifying

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ import twitter
 # TODO: fix videos with sound not being detected.
 # sample : https://twitter.com/YukaSlz/status/1371195200236834816
 
-# TODO: fix some videos not encoding properly
+# TODO: investigate images not being found in some tweets
+# sample : https://twitter.com/Junka_art/status/1372247107940147210
+# sample : https://twitter.com/TopsieTurvy_/status/1372928488802553856
+# sample : https://twitter.com/XaGueuzav/status/1372953005520211970
 
+# TODO: fix some videos not encoding properly
+# possible solution : https://stackoverflow.com/questions/32145166/get-video-from-tweet-using-twitter-api
 
 
 def get_api():
@@ -32,6 +37,13 @@ def get_api():
     )
 
 
+def get_status_info_from_url(url: str):
+    split_url = url.split("/")
+    status_id = split_url.pop(len(split_url) - 1)
+    user_id = split_url.pop(len(split_url) - 2)
+    return f"@{user_id}", status_id
+
+
 api = get_api()
 logger = []
 fail_logger = []
@@ -51,15 +63,43 @@ def open_input():
     return json.loads(data)
 
 
-def get_status_info_from_url(url: str):
-    split_url = url.split("/")
-    status_id = split_url.pop(len(split_url) - 1)
-    user_id = split_url.pop(len(split_url) - 2)
-    return f"@{user_id}", status_id
+def open_dev_input():
+    url_input = Path("test_input.json")
+
+    if Path(url_input).exists():
+        with open(url_input) as f:
+            data = f.read()
+    else:
+        print("test_input.json not found.")
+        return
+
+    return json.loads(data)
 
 
-def download_images(url: str):
+def get_folders_in_input():
+    json_file = open_dev_input()
+
+    for (folder, url) in json_file.items():
+        return str(folder), str(url)
+
+
+def download_images(input_data):
+    url = input_data[1]
+    target_folder = input_data[0]
+
     current_status = get_status_info_from_url(url)
+    path = Path("output")
+    if target_folder != "" or target_folder.__contains__("__none__"):
+        if target_folder.__contains__("__"):
+            temp_path = target_folder.split("__")
+            temp_path = temp_path[0]
+            full_path = f"{path}/{temp_path}"
+        else:
+            full_path = f"{path}/{target_folder}"
+
+        create_directory(full_path)
+    else:
+        full_path = path
 
     try:
         tweet_json = json.loads(api.GetStatus(current_status[1]).AsJsonString())
@@ -89,7 +129,7 @@ def download_images(url: str):
             media = requests.get(tweet_media)
             extension = str(tweet_media)[-3:]
 
-            with open(f"{author_name}.{extension}", 'wb') as f:
+            with open(f"{full_path}/{author_name}.{extension}", 'wb') as f:
                 f.write(media.content)
 
             print(f"{tweet_media}, {author_name}")
@@ -98,33 +138,52 @@ def download_images(url: str):
             i = i + 1
 
 
-def multi_download_images(url_list):
-    for url in url_list:
-        download_images(url)
+def multi_download_images(input_data):
+    for (folder, url) in input_data.items():
+        download_images((str(folder), str(url)))
+
+
+def create_directory(path):
+    final_path = Path(path)
+
+    try:
+        os.mkdir(final_path)
+    except FileExistsError:
+        print(f'"{final_path}" already exists, doing nothing.')
+
+
+def create_directories():
+    path_output = Path("output")
+    path_logs = Path("logs")
+
+    create_directory(path_output)
+    create_directory(path_logs)
 
 
 def write_logs():
     now = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    path = Path("logs")
 
-    try:
-        os.mkdir("logs")
-    except FileExistsError:
-        print('"logs/" already exists, doing nothing.')
-
-    with open(f"logs/log_{now}.json", "w") as f:
+    with open(f"{path}/log_{now}.json", "w") as f:
         f.write(json.dumps(logger, indent=4))
 
     if len(fail_logger) > 0:
-        with open(f"logs/fails_log_{now}.json", "w") as f:
+        with open(f"{path}/fails_log_{now}.json", "w") as f:
             f.write(json.dumps(fail_logger, indent=4))
 
-    with open(f"logs/url_history_{now}.json", "w") as f:
+    with open(f"{path}/url_history_{now}.json", "w") as f:
         f.write(json.dumps(url_history, indent=4))
 
 
 print(json.dumps(open_input(), indent=4))
+create_directories()
 multi_download_images(open_input())
 write_logs()
+
+
+
+
+
 
 
 
